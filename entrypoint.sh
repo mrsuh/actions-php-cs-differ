2e82afe3889d26edd423763dfd849bb0b3281a88
Sukhachev Anton <mrsuh6@gmail.com>
2020-10-19 19:16:35 +0300

    Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ ARGUMENTS="$@"
 
 php /app/php-cs-fixer --version
 echo "## Running PHP CS Fixer with arguments «${ARGUMENTS}»"
-echo "PHP Version : ${PHP_FULL_VERSION}"
+echo "PHP Version: ${PHP_FULL_VERSION}"
+echo "Work directory:  $(pwd)"
 
 php /app/php-cs-fixer fix --allow-risky=yes --using-cache=no --dry-run --diff-format=udiff --config=/app/config.php --path-mode=intersection ${ARGUMENTS}
